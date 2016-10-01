7e67476804574ca800ccd2a4f871765222b65347
Andrew Yanchuk <tsoy.sl@mail.ru>
2016-10-01 13:59:58 +0300

    added two new files

---
+++ src/main/java/com/Andrew_Yanchuk/app/App.java
@@ -0,0 +1,13 @@
+package com.Andrew_Yanchuk.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
