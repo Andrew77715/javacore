dbe3a8b01ba59795aafd892d89cf5220ccca7834
Andrew Yanchuk <tsoy.sl@mail.ru>
2016-10-11 15:08:17 +0300

    home_work_3

---
+++ src/main/java/main/java/com/Andrew77715/utils/runners/Lesson3Runner.java
@@ -0,0 +1,11 @@
+package main.java.com.Andrew77715.utils.runners;
+
+
+public class Lesson3Runner {
+    public static void main (String args[]){
+        PrimitiveConverter convertor = new PrimitiveConverter();
+        convertor.charToInt('q');
+        convertor.floatToChar('1');
+        convertor.intToChar('e');
+    }
+}
---
+++ src/main/java/main/java/com/Andrew77715/utils/runners/PrimitiveConverter.java
@@ -0,0 +1,19 @@
+package main.java.com.Andrew77715.utils.runners;
+
+
+public class PrimitiveConverter {
+    public void floatToChar(float input) {
+        char output = (char) input;
+        System.out.println("Input float value is " + input + "\nOutput char value is " + output);
+    }
+
+    public void charToInt(char input) {
+        int output = (int) input;
+        System.out.println("Input char value is " + input + "\nOutput int value is " + output);
+    }
+
+    public void intToChar(int input) {
+        char output = (char) input;
+        System.out.println("Input int value is " + input + "\nOutput char value is " + output);
+    }
+}
