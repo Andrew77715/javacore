0775cfb235c610d96755b4ca358cd8732469916d
Andrew Yanchuk <tsoy.sl@mail.ru>
2016-10-12 21:14:50 +0300

    change_file

--- src/main/java/com/Andrew_Yanchuk/app/PrimitiveConverter.java
+++ src/main/java/com/Andrew_Yanchuk/app/PrimitiveConverter.java
@@ -1,19 +1,22 @@
-package main.java.com.Andrew77715.utils.runners;
+package com.Andrew_Yanchuk.app;
 
 
 public class PrimitiveConverter {
-    public void floatToChar(float input) {
+    public char floatToChar(float input) {
         char output = (char) input;
         System.out.println("Input float value is " + input + "\nOutput char value is " + output);
+        return output;
     }
 
-    public void charToInt(char input) {
+    public int charToInt(char input) {
         int output = (int) input;
         System.out.println("Input char value is " + input + "\nOutput int value is " + output);
+        return output;
     }
 
-    public void intToChar(int input) {
+    public char intToChar(int input) {
         char output = (char) input;
         System.out.println("Input int value is " + input + "\nOutput char value is " + output);
+        return output;
     }
 }
--- src/main/java/main/java/com/Andrew77715/utils/runners/Lesson3Runner.java
+++ src/main/java/main/java/com/Andrew77715/utils/runners/Lesson3Runner.java
@@ -1,11 +1,11 @@
 package main.java.com.Andrew77715.utils.runners;
-
+import com.Andrew_Yanchuk.app.PrimitiveConverter;
 
 public class Lesson3Runner {
     public static void main (String args[]){
         PrimitiveConverter convertor = new PrimitiveConverter();
         convertor.charToInt('q');
-        convertor.floatToChar('1');
+        convertor.floatToChar('2');
         convertor.intToChar('e');
     }
 }
